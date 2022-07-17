089eed756bf64c38df0eb8e0bb38212c40f7be89
Suraj Babu <sbt@surajs-air.myfiosgateway.com>
2022-07-17 13:20:10 -0400

    One py file created in new branch

---
+++ file1.py
@@ -0,0 +1,8 @@
+# Calculation for the Addition and Substraction
+
+a = int(input("Enter the first random number:  "))
+b = int(input("Enter the second random number:  "))
+
+additn = a + b
+
+print(additn)
